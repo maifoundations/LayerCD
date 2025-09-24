097d8cd6e7ae36e27dec723eca679e661cc04d29
Bingkui Tong <tbbbk@qq.com>
2025-09-24 20:54:14 +0400

    remove shell and modify readme

--- shell/intuition.sh
+++
@@ -1,7 +0,0 @@
-# !/bin/bash
-
-for value in {1..23}
-do
-    echo "LLaVA MCQ, SELECTED_LAYER_LLAVA=$value"
-    CUDA_VISIBLE_DEVICES=0,1,2,3 /home/bingkui/miniconda3/envs/cambrian/bin/python intuition.py --dataset=MCQ --batch_size=64  --model_type=LLaVA --selected_layer=$value
-done
